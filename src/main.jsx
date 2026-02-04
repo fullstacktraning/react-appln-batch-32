493a3ebbc83978e82b8853b8c2ca9dc6d5c7ad35
React <samba.pendela@gmail.com>
2026-02-04 10:06:42 +0530

    code

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Comp1 from './components/Comp1.jsx'
+import Comp3 from './components/Comp3.jsx'
+createRoot(document.getElementById('root')).render(
+   <Comp3></Comp3>,
+)
