af97328f75abbad9e31b647dad6af84265b68148
Samba <samba.pendela@gmail.com>
2026-02-10 10:14:22 +0530

    Get Calls

--- src/main.jsx
+++ src/main.jsx
@@ -16,6 +16,8 @@ import Parent3 from './components/Parent3.jsx'
 import Parent4 from "./components/Parent4.jsx"
 import Parent5 from './components/Parent5.jsx'
 import GetEx1 from './components/GetEx1.jsx'
+import GetEx2 from './components/GetEx2.jsx'
+import GetEx3 from './components/GetEx3.jsx'
 createRoot(document.getElementById('root')).render(
-   <GetEx1></GetEx1>,
+   <GetEx3></GetEx3>,
 )
