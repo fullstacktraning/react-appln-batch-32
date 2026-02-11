8811b497b969838352da9677e176317733238afb
samba <samba.pendela@gmail.com>
2026-02-11 09:42:37 +0530

    code

--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,7 @@ import Parent5 from './components/Parent5.jsx'
 import GetEx1 from './components/GetEx1.jsx'
 import GetEx2 from './components/GetEx2.jsx'
 import GetEx3 from './components/GetEx3.jsx'
+import GetEx4 from './components/GetEx4.jsx'
 createRoot(document.getElementById('root')).render(
-   <GetEx3></GetEx3>,
+   <GetEx4></GetEx4>,
 )
